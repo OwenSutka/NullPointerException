89c2824fcd13d084cb3999360ede189ec65c8cc5
lrc035 <37704259+lrc035@users.noreply.github.com>
2020-02-01 12:26:05 -0600

    Add function placeholders to buttons

--- mitty/src/Home.js
+++ mitty/src/Home.js
@@ -2,22 +2,39 @@ import React, { Component } from "react";
 
 class Home extends Component {
 
+	constructor(props) {
+		super(props);
+		this.fileSystem.bind(this);
+	}
+
 	fileSystem () {
-		console.log("File System Selected")
+		alert('File System');
+	}
+
+	clipboard () {
+		alert('Clipboard');
+	}
+
+	snippingTool () {
+		alert('Snipping Tool');
+	}
+
+	camera () {
+		alert('Camera');
 	}
 
 
 
 	render () {
 		return (
-			<div class="home-group">
+			<div className="home-group">
 				<h2>Home</h2>
 				<p>Import an Image of Your Equation:</p>
-				<div class="import-group">
-					<button>File System</button>
-					<button>Clipboard</button>
-					<button>Snipping Tool</button>
-					<button>Camera</button>
+				<div className="import-group">
+					<button onClick ={this.fileSystem}>File System</button>
+					<button onClick ={this.clipboard}>Clipboard</button>
+					<button onClick ={this.snippingTool}>Snipping Tool</button>
+					<button onClick ={this.camera}>Camera</button>
 				</div>
 			</div>
 		);
