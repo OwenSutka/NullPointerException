02edb57be9493212acf204731dac3064891adc27
EboniWilliams <maiijae.ew@gmail.com>
2020-02-02 07:41:31 -0600

    added popup text when function done

--- mitty/src/Home.js
+++ mitty/src/Home.js
@@ -4,26 +4,48 @@ class Home extends Component {
 
 	constructor(props) {
 		super(props);
-		this.fileSystem.bind(this);
+		this.state = {
+			hasImage: false,
+			output: ""
+		};
+		this.output = "";
+		this.fileSystem = this.fileSystem.bind(this);
+		this.clipboard = this.clipboard.bind(this);
+		this.snippingTool = this.snippingTool.bind(this);
+		this.camera = this.camera.bind(this);
 	}
 
 	fileSystem () {
 		alert('File System');
+		this.setState({
+			hasImage: true,
+			output: "Image obtained from fileSystem"
+		});
 	}
 
 	clipboard () {
 		alert('Clipboard');
+		this.setState({
+			hasImage: true,
+			output: "Image obtained from clipboard"
+		});
 	}
 
 	snippingTool () {
 		alert('Snipping Tool');
+		this.setState({
+			hasImage: true,
+			output: "Image obtained from snippingTool"
+		});
 	}
 
 	camera () {
 		alert('Camera');
+		this.setState({
+			hasImage: true,
+			output: "Image obtained from camera"
+		});
 	}
-
-
 
 	render () {
 		return (
@@ -35,6 +57,12 @@ class Home extends Component {
 					<button onClick ={this.clipboard}>Clipboard</button>
 					<button onClick ={this.snippingTool}>Snipping Tool</button>
 					<button onClick ={this.camera}>Camera</button>
+				</div>
+				<div className="image-preview">
+					{this.state.hasImage ? <p>{this.state.output}</p> : null}
+				</div>
+				<div className="result-output">
+
 				</div>
 			</div>
 		);
