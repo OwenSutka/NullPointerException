3642eb1687bb6e2848a775349feb8aee3167e62b
lrc035 <37704259+lrc035@users.noreply.github.com>
2020-02-03 00:14:02 -0600

    Updating CSS

--- mitty/src/Main.js
+++ mitty/src/Main.js
@@ -21,23 +21,21 @@ class Main extends Component {
 	render() {
 		return (
 			<HashRouter>
-				<div>
-					<div className="main">
-						<h1>MITTY</h1>
-						<ul className="header">
-							<li><NavLink to="/">Home</NavLink></li>
-							<li><NavLink to="/about">About</NavLink></li>
-							<li><NavLink to="/howItWorks">How It Works</NavLink></li>
-							<li><NavLink to="/resources">Resources</NavLink></li>
-							<li><NavLink to="/contactUs">Contact Us</NavLink></li>
-						</ul>
-						<div className="content">
-							<Route path="/about" component={About}/>
-							<Route path="/howItWorks" component={HowItWorks}/>
-							<Route path="/resources" component={Resources}/>
-							<Route path="/contactUs" component={ContactUs}/>
-							<Route exact path="/" component={Home}/>
-						</div>
+				<div className="main">
+					<h1>MITTY</h1>
+					<ul className="header">
+						<li><NavLink to="/">Home</NavLink></li>
+						<li><NavLink to="/about">About</NavLink></li>
+						<li><NavLink to="/howItWorks">How It Works</NavLink></li>
+						<li><NavLink to="/resources">Resources</NavLink></li>
+						<li><NavLink to="/contactUs">Contact Us</NavLink></li>
+					</ul>
+					<div className="content">
+						<Route path="/about" component={About}/>
+						<Route path="/howItWorks" component={HowItWorks}/>
+						<Route path="/resources" component={Resources}/>
+						<Route path="/contactUs" component={ContactUs}/>
+						<Route exact path="/" component={Home}/>
 					</div>
 				</div>
 			</HashRouter>
